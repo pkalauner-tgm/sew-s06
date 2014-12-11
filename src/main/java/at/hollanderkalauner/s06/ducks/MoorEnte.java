a5d14cbd94ab8bb26343cc2a9d4c212e47a173ba
ReneHollander <rene.hollander@hotmail.de>
2014-12-11 12:05:08 +0100

    added methods to quackfaehig added some ducks

---
+++ src/main/java/at/hollanderkalauner/s06/ducks/MoorEnte.java
@@ -0,0 +1,24 @@
+package at.hollanderkalauner.s06.ducks;
+
+import at.hollanderkalauner.s06.interfaces.Beobachter;
+import at.hollanderkalauner.s06.interfaces.Quakfaehig;
+
+/**
+ * Created by rene on 12/11/14.
+ */
+public class MoorEnte implements Quakfaehig {
+    @Override
+    public void quaken() {
+
+    }
+
+    @Override
+    public void benachrichtigeBeobachtende() {
+
+    }
+
+    @Override
+    public void registriereBeobachter(Beobachter beobachter) {
+
+    }
+}
--- src/main/java/at/hollanderkalauner/s06/interfaces/Quakfaehig.java
+++ src/main/java/at/hollanderkalauner/s06/interfaces/Quakfaehig.java
@@ -4,4 +4,11 @@ package at.hollanderkalauner.s06.interfaces;
  * Created by Paul on 11.12.2014.
  */
 public interface Quakfaehig {
+
+    public void quaken();
+
+    public void benachrichtigeBeobachtende();
+
+    public void registriereBeobachter(Beobachter beobachter);
+
 }
